ca1b23818a576ca38b0e0a995b0e2b1e9c70455f
y-masaya <my.927ac@gmail.com>
2019-08-31 20:14:45 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FishMarket
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.test_framework false
+    end
+  end
+end
